9a39d8d7526634b4fa3096892c53acd20aa5bb03
Ujjawal Srivastava <ujjawal04srivastava@gmail.com>
2022-01-10 15:29:38 +0530

    commit changess for 10/1/22

--- src/main/java/com/vpinfotech/aws/dao/StorageRepository.java
+++ src/main/java/com/vpinfotech/aws/dao/StorageRepository.java
@@ -3,7 +3,6 @@ package com.vpinfotech.aws.dao;
 import com.vpinfotech.aws.entity.ImageStorage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StorageRepository extends JpaRepository<ImageStorage,Integer>
-{
+public interface StorageRepository extends JpaRepository<ImageStorage,Integer> {
 
 }
